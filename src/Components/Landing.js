e082ec813bf7f1ffba11a5e3244ba3dd329ab89c
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-17 00:23:12 +0700

    :art: Created Navbar and Footer
    .

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import '../Styles/Landing.css';
-
+import Logo from '../Utils/logo.png'
 
 class Landing extends Component {
   render() {
     return (
       <div className="Landing">
         <h1>This is Landing component</h1>
+        <img src={Logo} alt="KMUTNB CSDAY 2018" height="400px"/>
       </div>
     );
   }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../Styles/Navbar.css';
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="Navbar">
+        <h1>This is Navbar component</h1>        
+      </div>
+    );
+  }
+}
+
+export default Navbar;
