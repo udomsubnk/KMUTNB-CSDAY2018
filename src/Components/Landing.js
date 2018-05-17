bf0f958ed5d5a70dd90b0098e6c06e92de4e6f45
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-17 12:28:23 +0700

    :art: Change styles

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -10,13 +10,15 @@ class Landing extends Component {
       <div className="Landing">
         <img src={Logo} alt="KMUTNB CSDAY 2018" width="230px" className="Landing-logo"/>
         <h1>KMUTNB CSDAY 2018</h1>
-        <h1>เตรียมพบกับ!! งานประกวดเทคโนโลยีสุดยิ่งใหญ่</h1>
-        <h2>ของนักศึกษาภาควิชาคอมพิวเตอร์ มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h2>
+        <h1 className="Landing-preparetext">เตรียมพบกับ!! งานประกวดเทคโนโลยีสุดยิ่งใหญ่</h1>
+        <h2>ของ<span className="Landing-csstudent">นักศึกษาภาควิชาคอมพิวเตอร์</span> มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h2>
         <Carousel />
         <div className="Landing-calltoaction">
           <Button outline color="primary" size="lg" className="Landing-register" href="https://docs.google.com/forms/d/e/1FAIpQLSd1MoDN5sd6t0pDZRE85aePMoUm4aTjKObXAUP6EohqAqsKKw/viewform" target="_blank">
             ลงทะเบียนเข้าร่วมงาน
           </Button>
+          <br />
+          <span className="Landing-register-text">สำหรับหน่วยงานหรือบริษัทที่สนใจเข้าเยี่ยมชม</span>
         </div>
       </div>
     );
