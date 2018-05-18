637ece9589c130e072768719179a890fb33b1f6a
kukiat <kukiat1996@gmail.com>
2018-05-19 00:15:36 +0700

    bind scroll to nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
 import '../Styles/Navbar.css';
-
+import { Link } from 'react-scroll'
 
 class Navbar extends Component {
   render() {
     return (
     <div className="nav">
       <div className="nav-menu">
-        <a className="nav-ladding" href="">Laning</a>
-        <a className="nav-date" href="">Date</a>
-        <a className="nav-detail" href="">Detail</a>
-        <a className="nav-location" href="">Location</a>
+        <a className="nav-landing" href="">
+          <Link 
+            activeClass="active"
+            spy={true} 
+            smooth={true} 
+            duration={500}
+            to="scroll-landing"
+          >
+            landing
+          </Link>
+        </a>
+        <a className="nav-date" href="">
+          <Link 
+            activeClass="active"
+            spy={true} smooth={true} duration={500}
+            to="scroll-date"
+          >
+            date
+          </Link>
+        </a>
+        <a className="nav-detail" href="">
+          <Link 
+            activeClass="active"
+            spy={true} smooth={true} duration={500}
+            to="scroll-detail"
+          >
+            detail
+          </Link>
+        </a>
+        <a className="nav-location" href="">
+          <Link 
+            activeClass="active"
+            spy={true} smooth={true} duration={500}
+            to="scroll-location"
+          >
+            location
+          </Link>
+        </a>
       </div>
     </div>
     );
