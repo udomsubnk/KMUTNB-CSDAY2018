02ecc32aa34a050b48255e6b8426018a62c27a3e
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-18 10:53:21 +0700

    :art: Set style

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,11 +7,13 @@ class Landing extends Component {
   render() {
     return (
       <div className="Landing">
-        <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="180px" className="Landing-logo"/>
-        <h1>KMUTNB CSDAY 2018</h1>
-        <h1 className="Landing-preparetext">เตรียมพบกับ!! งานประกวดเทคโนโลยีสุดยิ่งใหญ่</h1>
-        <h2 className="inline">ของ<span className="Landing-csstudent">นักศึกษาภาควิชาคอมพิวเตอร์</span></h2>
-        <h2 className="inline">&nbsp;มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h2>
+        <div className="Landing-head">
+          <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="180px" className="Landing-logo"/>
+          <h1>KMUTNB CSDAY 2018</h1>
+          <h1 className="Landing-preparetext">เตรียมพบกับ!! งานประกวดเทคโนโลยีสุดยิ่งใหญ่</h1>
+          <h2 className="inline">ของ<span className="Landing-csstudent">นักศึกษาภาควิชาคอมพิวเตอร์</span></h2>
+          <h2 className="inline">&nbsp;มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h2>
+        </div>
         <Carousel />
         <div className="Landing-calltoaction">
           <Button outline color="primary" size="lg" className="Landing-register" href="https://docs.google.com/forms/d/e/1FAIpQLSd1MoDN5sd6t0pDZRE85aePMoUm4aTjKObXAUP6EohqAqsKKw/viewform" target="_blank">
