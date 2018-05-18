2be6f7e4c723e7165639756014574ae8a6a11cc0
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-18 19:36:47 +0700

    finished base Date component

--- src/Components/DateAndTime.js
+++ src/Components/DateAndTime.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
 import '../Styles/DateAndTime.css';
 
-class DateAndTime extends Component {
+export default class DateAndTime extends Component {
   state = {
     activities: [
       {
-        time: "10:00",
+        time: "10:01",
         activity: "Start"
       }, {
-        time: "10:00",
-        activity: "Start"
+        time: "10:02",
+        activity: "Start Again"
       }, {
-        time: "10:00",
-        activity: "Start"
+        time: "10:03",
+        activity: "Start Again And Again"
       }, {
-        time: "10:00",
-        activity: "Start"
+        time: "10:04",
+        activity: "Start Again And Again And Again"
       }
     ]
   }
   render() {
+    const {activities} = this.state;
+    const activityItems = activities.map(({activity, time}) => (
+      <div className="time-table-item" key={time}>
+        <div className="time">
+          <h4 data-aos="fade-right" style={{
+            margin: 0
+          }}>{time}</h4>
+        </div>
+        <div className="activity">
+          <h5 data-aos="fade-left" style={{
+            margin: 0
+          }}>{activity}</h5>
+        </div>
+      </div>
+    ))
+
     return (
       <div className="DateAndTime" data-aos="fade-up">
         <div className="title-section">
@@ -27,28 +43,9 @@ class DateAndTime extends Component {
           <h2>28 June 2018</h2>
         </div>
         <div className="time-table">
-          <div className="left-side">
-            {this
-              .state
-              .activities
-              .map(({activity, time}) => (
-                <div class="time">{time}</div>
-              ))}
-
-          </div>
-          <div className="right-side">
-            {this
-              .state
-              .activities
-              .map(({activity, time}) => (
-                <div class="activity">{activity}</div>
-              ))}
-          </div>
+          {activityItems}
         </div>
-
       </div>
-    );
+    )
   }
 }
-
-export default DateAndTime;
