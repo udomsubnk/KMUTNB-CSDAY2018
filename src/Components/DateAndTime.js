5e4db1f102e5a939c18466a9df9d7604e973b1f0
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-18 18:42:59 +0700

    added timetable

--- src/Components/DateAndTime.js
+++ src/Components/DateAndTime.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../Styles/DateAndTime.css';
 
 class DateAndTime extends Component {
+  state = {
+    activities: [
+      {
+        time: "10:00",
+        activity: "Start"
+      }, {
+        time: "10:00",
+        activity: "Start"
+      }, {
+        time: "10:00",
+        activity: "Start"
+      }, {
+        time: "10:00",
+        activity: "Start"
+      }
+    ]
+  }
   render() {
     return (
       <div className="DateAndTime" data-aos="fade-up">
-        <h1>This is DateAndTime component</h1>
+        <div className="title-section">
+          <h1>DATE AND TIME</h1>
+          <h2>28 June 2018</h2>
+        </div>
+        <div className="time-table">
+          <div className="left-side">
+            {this
+              .state
+              .activities
+              .map(({activity, time}) => (
+                <div class="time">{time}</div>
+              ))}
+
+          </div>
+          <div className="right-side">
+            {this
+              .state
+              .activities
+              .map(({activity, time}) => (
+                <div class="activity">{activity}</div>
+              ))}
+          </div>
+        </div>
+
       </div>
     );
   }
