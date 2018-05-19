3c095efdd707a7dd20950767216376399424a76d
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-19 23:45:51 +0700

    :art: Create CisAndLogo for Logo of CIS and KMUTNB

---
+++ src/Components/CisAndSocial.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../Styles/CisAndSocial.css';
+
+export default class CisAndSocial extends Component {
+    render() {
+        return (
+            <div className="CisAndSocial">
+                
+            </div>
+        ); 
+    }
+}
