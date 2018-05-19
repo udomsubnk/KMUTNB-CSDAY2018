6e8e87d52c830a678d688f6b493e63cf48664d8b
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-20 01:57:20 +0700

    :lipstick: Set style of content in Detail

--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -7,10 +7,25 @@ class Detail extends Component {
   render() {
     return (
       <Element className="mgt-global-20" name="scroll-detail">
-        <div className="Detail Detail-textalign-left" data-aos="fade-up">
-          <h1>WHAT IS CS DAY?</h1>     
-          <h1>PARTICIPANTS</h1>
-          <h1>REASONS TO COME</h1>
+        <div className="Detail Detail-textalign-left">
+          <div>
+            <h1 data-aos="fade-up" className="web-color">WHAT IS CS DAY?</h1>   
+            <p data-aos="fade-up" className="Detail-content">
+              HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+            </p>  
+          </div>
+          <div className="Detail-mgtitem">
+            <h1 data-aos="fade-up" className="other-color">PARTICIPANTS</h1>
+            <p data-aos="fade-up" className="Detail-content">
+              HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+            </p>  
+          </div>
+          <div className="Detail-mgtitem">
+            <h1 data-aos="fade-up" className="ml-color">REASONS TO COME</h1>
+            <p data-aos="fade-up" className="Detail-content">
+              HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello
+            </p>  
+          </div>
         </div>
       </Element>
     );
