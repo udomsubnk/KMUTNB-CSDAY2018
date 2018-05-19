388cd36f0b96a14842328219d88cfb2c0759a679
kukiat <kukiat1996@gmail.com>
2018-05-19 23:27:40 +0700

    hover in nav & fix display when show

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ class Navbar extends Component {
     if(window.scrollY < 200) {
       document.getElementById('nav-bar').style.display = 'none'
     }else {
-      document.getElementById('nav-bar').style.display = 'block'
+      document.getElementById('nav-bar').style.display = ''
     }
   }
 
@@ -27,15 +27,23 @@ class Navbar extends Component {
     return (
     <div id='nav-bar' className="nav">
       <div className="nav-menu">
-        <NavMenu name='landing'>landing</NavMenu>
-        <NavMenu name='date'>date & time</NavMenu>
+        <NavMenu name='landing'>
+          <p className="nav-text">landing</p>
+        </NavMenu>
+        <NavMenu name='date'>
+          <p className="nav-text">date & time</p>
+        </NavMenu>
         <NavMenu name='landing'>
           <div className="nav-logo">
-              <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="80px" className=""/>
+              <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="80px" />
           </div>
         </NavMenu>
-        <NavMenu name='detail'>detail</NavMenu>
-        <NavMenu name='location'>location</NavMenu>
+        <NavMenu name='detail'>
+          <p className="nav-text">detail</p>
+        </NavMenu>
+        <NavMenu name='location'>
+          <p className="nav-text">location</p>
+        </NavMenu>
       </div>
       <div className="hamberger">
         <HambergerNav/>
