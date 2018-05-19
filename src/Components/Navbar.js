842afaf74f3961d9536a8b323b228443810b05fa
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-19 13:08:35 +0700

    :lipstick: Set style & animaton in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,16 +6,13 @@ import HambergerNav from './HambergerNav'
 import $ from 'jquery';
 class Navbar extends Component {
   render() {
-
     return (
     <div className="nav">
       <div className="nav-menu">
         <NavMenu name='landing'>landing</NavMenu>
         <NavMenu name='date'>date</NavMenu>
         <div className="nav-logo">
-          <a href="http://th.spankbang.com/7x91/video/063+sw251">
-            <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="50px" className=""/>
-          </a>
+            <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="80px" className=""/>
         </div>
         <NavMenu name='detail'>detail</NavMenu>
         <NavMenu name='location'>location</NavMenu>
@@ -33,6 +30,9 @@ $(document).ready(()=>{
       $('.nav').hide()
     }
     else $('.nav').show()
+  })
+  $('.nav-logo').click(()=>{
+    $('html,body').animate({ scrollTop: 0 }, 'slow');
   })
 })
 export default Navbar;
