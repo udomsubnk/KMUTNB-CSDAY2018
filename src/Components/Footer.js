971692eafbce40f3e6305e8782b38b201870e8e8
kukiat <kukiat1996@gmail.com>
2018-05-20 02:25:22 +0700

    fix name in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
       <div className="Footer">
         <a href="https://www.facebook.com/udomsub.nk" target="_blank" rel="noopener noreferrer">@udomsubnk</a>&nbsp;
         <a href="https://www.facebook.com/Basone01" target="_blank" rel="noopener noreferrer">@Basone01</a>&nbsp;
-        <a href="https://www.facebook.com/kukiat.wangtaphan" target=" _blank" rel="noopener noreferrer">@kukiat</a>&nbsp;
+        <a href="https://www.facebook.com/kukiat.wangtaphan" target=" _blank" rel="noopener noreferrer">@kukiat5234</a>&nbsp;
         <a href="https://www.facebook.com/aiyhero" target="_blank" rel="noopener noreferrer">@blaster666</a>
       </div>
     );
