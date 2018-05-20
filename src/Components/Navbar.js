050252dd7f0239b5c561774eb3fc72c4fcc1b7ea
kukiat <kukiat1996@gmail.com>
2018-05-21 02:18:25 +0700

    fix more

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -28,35 +28,19 @@ class Navbar extends Component {
       <div className="nav-menu">
         <NavItem name='landing'>
           <p className="nav-text">landing</p>
-<<<<<<< HEAD
         </NavItem>
-        <NavItem name='date'>
-          <p className="nav-text">date & time</p>
+        <NavItem name='detail'>
+          <p className="nav-text">detail</p>
         </NavItem>
         <NavItem name='landing'>
           <div className="nav-logo">
               <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="80px" />
           </div>
         </NavItem>
-        <NavItem name='detail'>
-          <p className="nav-text">detail</p>
+        <NavItem name='table'>
+          <p className="nav-text">Project lists</p>
         </NavItem>
         <NavItem name='location'>
-=======
-        </NavMenu>
-        <NavMenu name='detail'>
-          <p className="nav-text">detail</p>
-        </NavMenu>
-        <NavMenu name='landing'>
-          <div className="nav-logo">
-              <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="80px" />
-          </div>
-        </NavMenu>
-        <NavMenu name='table'>
-          <p className="nav-text">Project lists</p>
-        </NavMenu>
-        <NavMenu name='location'>
->>>>>>> 60d7171add8cc298213040f75620e832b706bc83
           <p className="nav-text">location</p>
         </NavItem>
         
