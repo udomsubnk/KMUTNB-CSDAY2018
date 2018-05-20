47545bdf340e2ebfdd36122e1b77a71d4a45901c
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-20 16:18:00 +0700

    :lipstick: Carousel use parallax

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,23 +3,33 @@ import '../Styles/Landing.css';
 import { Button } from 'reactstrap';
 import Carousel from './Carousel.js'
 import { Element } from 'react-scroll'
+import Parallax from 'parallax-js';
 
 class Landing extends Component {
+  componentDidMount() {
+    var scene = document.getElementById('parallax');
+    var parallaxInstance = new Parallax(scene);
+  }
+
   render() {
     return (
-      <Element name="scroll-time">      
+      <Element name="scroll-time">
         <div className="Landing" data-aos="fade-up">
           <div className="Landing-head">
-            <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="180px" className="Landing-logo"/>
+            <img src="/logo.png" alt="KMUTNB CSDAY 2018" width="180px" className="Landing-logo" />
             <h1 className="Landing-title">KMUTNB CSDAY 2018</h1>
             <h1 className="Landing-preparetext">เตรียมพบกับ!! งานประกวดเทคโนโลยีสุดยิ่งใหญ่</h1>
             <h2 className="inline">ของ<span className="Landing-csstudent">นักศึกษาภาควิชาคอมพิวเตอร์</span></h2>
             <h2 className="inline">&nbsp;มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</h2>
           </div>
-          <Carousel />
+          <div id="parallax">
+            <div data-depth="0.3">
+              <Carousel />
+            </div>
+          </div>
           <div className="Landing-calltoaction">
-            <Button outline color="primary" size="lg" className="Landing-register" 
-              href="https://docs.google.com/forms/d/e/1FAIpQLSd1MoDN5sd6t0pDZRE85aePMoUm4aTjKObXAUP6EohqAqsKKw/viewform" 
+            <Button outline color="primary" size="lg" className="Landing-register"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSd1MoDN5sd6t0pDZRE85aePMoUm4aTjKObXAUP6EohqAqsKKw/viewform"
               target="_blank"
             >
               ลงทะเบียนเข้าร่วมงาน
