b3619673e267afeff2f195e23ffb48223b26abfd
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-20 20:25:07 +0700

    :art: Fix to responsive

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,10 +5,10 @@ class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <a href="https://www.facebook.com/udomsub.nk" target="_blank" rel="noopener noreferrer">@udomsubnk</a>&nbsp;
-        <a href="https://www.facebook.com/Basone01" target="_blank" rel="noopener noreferrer">@Basone01</a>&nbsp;
-        <a href="https://www.facebook.com/kukiat.wangtaphan" target=" _blank" rel="noopener noreferrer">@kukiat5234</a>&nbsp;
-        <a href="https://www.facebook.com/aiyhero" target="_blank" rel="noopener noreferrer">@blaster666</a>
+        <div className="inline"><a href="https://www.facebook.com/udomsub.nk" target="_blank" rel="noopener noreferrer">@udomsubnk</a>&nbsp;</div>
+        <div className="inline"><a href="https://www.facebook.com/Basone01" target="_blank" rel="noopener noreferrer">@Basone01</a>&nbsp;</div>
+        <div className="inline"><a href="https://www.facebook.com/kukiat.wangtaphan" target=" _blank" rel="noopener noreferrer">@kukiat5234</a>&nbsp;</div>
+        <div className="inline"><a href="https://www.facebook.com/aiyhero" target="_blank" rel="noopener noreferrer">@blaster666</a></div>
       </div>
     );
   }
--- src/Components/StarfieldCanvas.js
+++ src/Components/StarfieldCanvas.js
@@ -28,6 +28,7 @@ class StarfieldCanvas extends Component {
 					bottom: 0,
 					right: 0,
 					width: '100%',
+					maxWidth:'100vw',
 					height: '100%',
 					objectFit: 'cover',
 					opacity: 0.8
