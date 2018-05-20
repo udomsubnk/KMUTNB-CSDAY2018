daf4e5a1340b47c0a5636c69e9a1a03f07f7aba6
DESKTOP-OK4OG5A\Basone01 <basone01@hotmail.com>
2018-05-20 15:18:11 +0700

    removed unused method

--- src/Components/StarfieldCanvas.js
+++ src/Components/StarfieldCanvas.js
@@ -1,45 +1,39 @@
-import React, {Component} from 'react'
-import Starfield from 'canvas-starfield'
+import React, { Component } from 'react';
+import Starfield from 'canvas-starfield';
 
 class StarfieldCanvas extends Component {
-    state = {
-        starfield: null
-    }
-    componentDidMount() {
-        const starfield = new Starfield({
-            canvas: '#starfield',
-            numStars: 400,
-            maxRadius: 2,
-            shootingStarInterval: 2
-        })
-        starfield.start()
-        this.setState({starfield})
+	state = {
+		starfield: null
+	};
+	componentDidMount() {
+		const starfield = new Starfield({
+			canvas: '#starfield',
+			numStars: 400,
+			maxRadius: 2,
+			shootingStarInterval: 2
+		});
+		starfield.start();
+		this.setState((state) => ({ starfield }));
+	}
 
-    }
-    componentWillUnmount() {
-        this
-            .state
-            .starfield
-            .stop();
-    }
-
-    render() {
-        return (
-            <canvas
-                id="starfield"
-                style={{
-                zIndex: -1,
-                position: "fixed",
-                top: 0,
-                left: 0,
-                bottom: 0,
-                right: 0,
-                width: "100%",
-                height: "100%",
-                objectFit:"cover"
-            }}></canvas>
-        )
-    }
+	render() {
+		return (
+			<canvas
+				id="starfield"
+				style={{
+					zIndex: -1,
+					position: 'fixed',
+					top: 0,
+					left: 0,
+					bottom: 0,
+					right: 0,
+					width: '100%',
+					height: '100%',
+					objectFit: 'cover'
+				}}
+			/>
+		);
+	}
 }
 
-export default StarfieldCanvas
+export default StarfieldCanvas;
