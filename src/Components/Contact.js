3b367050dd47fc2ff124a1e55d2f1d45b6c1e39f
Udomsub Naikorn <udomsub.nk@gmail.com>
2018-05-20 19:33:30 +0700

    :speech_balloon: Update text in Contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,7 +7,11 @@ class Contact extends Component {
         return (
             <Element className="mgt-global-20" name="scroll-time">
                 <div className="Contact" data-aos="fade-up">
-                    <h1>This is Contact component</h1>
+                    <h2>ติดต่อสอบถามหรือสนับสนุนโครงการได้ที่</h2>
+                    <h4>พี่เก้า : <a href="tel:+66826472997">082-647-2997</a></h4>
+                    <h4>พี่พีช : <a href="tel:+66972491811">097-249-1811</a></h4>
+                    <h4>พี่พลอย : <a href="tel:+66822589402">082-258-9402</a></h4>
+                    <h4>พี่โฟม : <a href="tel:+66970714268">097-071-4268</a></h4>
                 </div>
             </Element>
         );
